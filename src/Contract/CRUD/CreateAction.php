fa81514c24d4ed2afa9f539aacac37388fbee555
ahmad_tahboub <ahmad.tahboob@gmail.com>
2022-03-14 10:02:17 +0300

    working

--- src/Contract/CRUD/CreateAction.php
+++ src/Contract/CRUD/CreateAction.php
@@ -5,8 +5,12 @@ use WabLab\Module\Standard\Contract\Entity;
 
 interface CreateAction 
 {
+    // Unit-test will be done in the concrete class since this method is related to it's own functionality
     public function createNewEntity(): static;
+    // Unit-test will be done in the concrete class since this method is related to it's own functionality
     public function fillInputsIntoEntity(): static;
+    
+    // Unit-test will be done using the abstract class since both below are the same with all of the actions
     public function saveEntity(): static;
-    public function returnEntity(Entity &$entity): static;
+    public function returnEntity(?Entity &$entity): static;
 }
--- src/CreateAction.php
+++ src/CreateAction.php
@@ -5,19 +5,21 @@ namespace WabLab\Module\Standard;
 use WabLab\Module\Standard\Contract\Entity;
 use WabLab\Module\Standard\Contract\Repository;
 
-abstract class CreateAction
-{
-    protected Entity $entity;
+abstract class CreateAction {
+    protected ?Entity $entity = null;
     protected Repository $repository;
-
-    public function __construct(Repository $repository)
-    {
+    
+    public function __construct(Repository $repository) {
         $this->repository = $repository;
     }
-
-    public function saveEntity(): static
-    {
+    
+    public function saveEntity(): static {
         $this->repository->save($this->entity);
+        return $this;
+    }
+    
+    public function returnEntity(?Entity &$entity): static {
+        $entity = $this->entity;
         return $this;
     }
 }
